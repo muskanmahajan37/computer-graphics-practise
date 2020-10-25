4a0beaa59e0b235ad571073282930b8aac3c1083
hulewiczkamil <hulewicz.k@gmail.com>
2020-10-25 20:18:30 +0100

    GOD, praise all the people sitting in front of PC, THANK YOU

--- Lab2/plasma_fractal.py
+++ Lab2/plasma_fractal.py
@@ -2,31 +2,57 @@
 import sys
 import numpy as np
 import random
-from scipy import norm
 
 from glfw.GLFW import *
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-m_size = 1025
+m_size = 5
 array_of_points = np.zeros((m_size,m_size))
 
-def initialize_array():
-    pass
+def initialize_array_monochromatic(m_size, array):
+    array[0][0] = randomize_color_monochromatic()
+    array[m_size-1][0] = randomize_color_monochromatic()
+    array[m_size-1][m_size-1] = randomize_color_monochromatic()
+    array[0][m_size-1] = randomize_color_monochromatic()
 
+def generate_other_vertecies(array, ax, ay, bx, by, cx, cy, dx, dy):
+    new_wx = ax
+    new_wy = (by-1-ay)//2
+    new_xx = cx-1
+    new_xy = (dy-1-cy)//2
+    new_yx = (cx-1-ax)//2
+    new_yy = ay
+    new_zx = (dx-1-bx)//2
+    new_zy = dy-1
+    new_qx = (cx-1-ax)//2
+    new_qy = (by-1-ay)//2
+    if(array[new_wx][new_wy] == 0): array[new_wx][new_wy] = randomize_color_monochromatic()
+    if(array[new_xx][new_xy] == 0): array[new_xx][new_xy] = randomize_color_monochromatic()
+    if(array[new_yx][new_yy] == 0): array[new_yx][new_yy] = randomize_color_monochromatic()
+    if(array[new_zx][new_zy] == 0): array[new_zx][new_zy] = randomize_color_monochromatic()
+    if(array[new_qx][new_qy] == 0): array[new_qx][new_qy] = randomize_color_monochromatic()
+
+    generate_other_vertecies
+    print(array)
+
+''' 
+    '''
 def startup():
     update_viewport(None, 400, 400)
     glClearColor(0.5, 0.5, 0.5, 1.0)
+    initialize_array_monochromatic(m_size, array_of_points)
+
+    generate_other_vertecies(array_of_points,0,0,0,m_size,m_size,0,m_size,m_size)
 
 
 def shutdown():
     pass
 
 def randomize_color_monochromatic():
-    color = random.randint(0,255) 
-    return [color for i in range(0,3)]
-
+    return random.uniform(0,1) 
+    
 def render(time):
  pass
 #glFlush()
@@ -74,7 +100,6 @@ def update_viewport(window, width, height):
 def main():
     if not glfwInit():
         sys.exit(-1)
-
     window = glfwCreateWindow(400, 400, __file__, None, None)
     if not window:
         glfwTerminate()
