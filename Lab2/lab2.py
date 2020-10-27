dd271af49d9fecaba7310d2725306a9a06ca048a
hulewiczkamil <hulewicz.k@gmail.com>
2020-10-27 11:18:39 +0100

    Color generation changed

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import sys
-import time as tajm
 
 from glfw.GLFW import *
 
@@ -10,21 +9,6 @@ import random
 
 lista = [0.0,1.0,1.0]
 
-class rectangle:
-
-    def __init__(x_of_center,y_of_center, width, height):
-        self.x1 = x_of_center + width/2
-        self.y1 = y_of_center + height/2
-        
-        self.x2 = x_of_center - width/2
-        self.y2 = y_of_center - height/2
-        
-        self.x3 = x_of_center + width/2
-        self.y3 = y_of_center - height/2
-        
-        self.x4 = x_of_center - width/2
-        self.y4 = y_of_center + height/2
-
 def startup():
     update_viewport(None, 400, 400)
     glClearColor(0.5, 0.5, 0.5, 1.0)
@@ -33,38 +17,35 @@ def shutdown():
     pass
 
 
-def draw_carpet(x_center, y_center, width, height, color, self_similarity = 0):
+def draw_carpet(x_center, y_center, width, height, self_similarity = 0):
     if self_similarity != 0:
-        draw_carpet(x_center - width/3, y_center+height/3, width/3, height/3, color,self_similarity-1)
-        draw_carpet(x_center, y_center+height/3, width/3, height/3, color, self_similarity-1)
-        draw_carpet(x_center + width/3, y_center+height/3, width/3, height/3, color, self_similarity-1)
-        draw_carpet(x_center - width/3, y_center, width/3, height/3, color, self_similarity-1)
-        draw_carpet(x_center + width/3, y_center, width/3, height/3, color, self_similarity-1)
-        draw_carpet(x_center - width/3, y_center-height/3, width/3, height/3, color, self_similarity-1)
-        draw_carpet(x_center, y_center-height/3, width/3, height/3, color, self_similarity-1)
-        draw_carpet(x_center + width/3, y_center-height/3, width/3, height/3, color, self_similarity-1)
+        draw_carpet(x_center - width/3, y_center+height/3, width/3, height/3, self_similarity-1)
+        draw_carpet(x_center, y_center+height/3, width/3, height/3, self_similarity-1)
+        draw_carpet(x_center + width/3, y_center+height/3, width/3, height/3, self_similarity-1)
+        draw_carpet(x_center - width/3, y_center, width/3, height/3, self_similarity-1)
+        draw_carpet(x_center + width/3, y_center, width/3, height/3, self_similarity-1)
+        draw_carpet(x_center - width/3, y_center-height/3, width/3, height/3, self_similarity-1)
+        draw_carpet(x_center, y_center-height/3, width/3, height/3, self_similarity-1)
+        draw_carpet(x_center + width/3, y_center-height/3, width/3, height/3, self_similarity-1)
     else:
-        if not color:
-            color = draw_rectangle(x_center, y_center, width, height, color)
-        draw_rectangle(x_center, y_center, width, height, color)
+        draw_rectangle(x_center, y_center, width, height)
 
 
-def draw_rectangle(x_of_center,y_of_center, width, height, color, d = 1):
+def draw_rectangle(x_of_center,y_of_center, width, height, color = [], d = 1):
     colors = []
-    if(not color):
+    if(not colors):
+        random.seed(x_of_center+y_of_center)
         for i in range(0,4):
             colors.append([
                         random.random(),
                         random.random(),
                         random.random()
                         ])
-        return colors
-    
     nodes = {
-        'node0': (x_of_center + width/2*d, y_of_center + height*d/2, color[0]),
-        'node1': (x_of_center - width/2*d, y_of_center + height/2*d, color[1]),
-        'node2': (x_of_center + width/2*d, y_of_center - height/2*d, color[2]),
-        'node3': (x_of_center - width/2*d, y_of_center - height/2*d, color[3])
+        'node0': (x_of_center + width/2*d, y_of_center + height*d/2, colors[0]),
+        'node1': (x_of_center - width/2*d, y_of_center + height/2*d, colors[1]),
+        'node2': (x_of_center + width/2*d, y_of_center - height/2*d, colors[2]),
+        'node3': (x_of_center - width/2*d, y_of_center - height/2*d, colors[3])
     }
 
     glBegin(GL_TRIANGLES)
@@ -89,11 +70,11 @@ def draw_rectangle(x_of_center,y_of_center, width, height, color, d = 1):
     return colors
 
 
-def render(time, colors):
+def render(time):
     glClear(GL_COLOR_BUFFER_BIT)
-    #colors = draw_rectangle(0,0,50.0,50.0, colors)
-    draw_carpet(0,0,150.0,150.0,[],int(sys.argv[1]))
-    tajm.sleep(1)
+    #draw_rectangle(0,0,50.0,50.0, colors,2.0)
+    #print(colors)
+    draw_carpet(0,0,150.0,150.0,int(sys.argv[1]))
 '''
     #Zadanie 1
     glColor3fv(lista)
@@ -133,7 +114,6 @@ def update_viewport(window, width, height):
 
 
 def main():
-    colors = []
     if not glfwInit():
         sys.exit(-1)
 
@@ -148,7 +128,7 @@ def main():
 
     startup()
     while not glfwWindowShouldClose(window):
-        render(glfwGetTime(), colors=colors)
+        render(glfwGetTime())
         glfwSwapBuffers(window)
         glfwPollEvents()
     shutdown()
